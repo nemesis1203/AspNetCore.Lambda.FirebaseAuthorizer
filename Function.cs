a54e94d4b4aef43fd14d9d56145df47b18ebb4a1
Raphael Bickel <raphael@7peakssoftware.com>
2018-05-03 11:45:43 +0700

    Initial commit

---
+++ Function.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.Auth.AccessControlPolicy;
+using Amazon.Auth.AccessControlPolicy.ActionIdentifiers;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Runtime.Internal;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace Lambda.Firebase.Authorizer
+{
+    public class Function
+    {
+
+        public static readonly string Issuer = Environment.GetEnvironmentVariable("ISSUER");
+        public static readonly string Audience = Environment.GetEnvironmentVariable("AUDIENCE");
+        private readonly ConfigurationManager<OpenIdConnectConfiguration> _configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>($"{Issuer}/.well-known/openid-configuration", new OpenIdConnectConfigurationRetriever());
+
+        public async Task<APIGatewayCustomAuthorizerResponse> FunctionHandler(APIGatewayCustomAuthorizerRequest authEvent, ILambdaContext context)
+        {
+            try
+            {
+                // validate the token -- checking auth should be filled in, current function just returns true for all tokens
+                var token = authEvent.AuthorizationToken;
+                (bool authorized, string username) = await CheckAuthorization(token);
+
+                return new APIGatewayCustomAuthorizerResponse
+                {
+                    PrincipalID = username,
+                    PolicyDocument = new APIGatewayCustomAuthorizerPolicy
+                    {
+                        Version = "2012-10-17",
+                        Statement = new AutoConstructedList<APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement>
+                        {
+                            new APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement
+                            {
+                                Action = new HashSet<string> {"execute-api:Invoke"},
+                                Resource = new HashSet<string> {authEvent.MethodArn},
+                                Effect = authorized ? "Allow" : "Deny"
+                            }
+                        }
+                    }
+                };
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error authorizing request. " + e.Message);
+                throw;
+            }
+
+        }
+        public virtual async Task<(bool, string)> CheckAuthorization(string token)
+        {
+            var configuration = await _configurationManager.GetConfigurationAsync(CancellationToken.None);
+            var validationParameters =
+                new TokenValidationParameters
+                {
+                    ValidIssuer = Issuer,
+                    ValidAudience = Audience,
+                    IssuerSigningKeys = configuration.SigningKeys
+                };
+
+            try
+            {
+                var user = new JwtSecurityTokenHandler().ValidateToken(token, validationParameters,
+                    out SecurityToken validatedToken);
+
+                return (true, user.Identity.Name);
+            }
+            catch
+            {
+                return (false, string.Empty);
+            }
+        }
+
+        public struct AuthorizerResult
+        {
+            [JsonProperty("principalId")]
+            public string PrincipalId { get; set; }
+
+            [JsonProperty("policyDocument")]
+            public APIGatewayCustomAuthorizerPolicy Policy { get; set; }
+
+        }
+
+    }
+}
+
+
+
+
